d89aaa01b8fd581407b040e410b56c01d69a527b
Rayan <rayanchambet1@gmail.com>
2022-12-22 19:46:04 +0100

    V.0.6.1.6
    Changed form handler to Netlify ✅

--- src/Components/ContactForm/index.jsx
+++ src/Components/ContactForm/index.jsx
@@ -1,38 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { useRef } from "react";
-// import emailjs from '@emailjs/browser';
 import "./ContactForm.scss"
 
 const ContactForm = () => {
-    const form = useRef();
     const [displayForm, setDisplayForm] = useState(true);
     const [emailSent, setEmailSent] = useState(false);
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
         const myForm = e.target;
         const formData = new FormData(myForm);
-
+    
         fetch("/", {
-            method:"POST",
-            headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: new URLSearchParams(formData).toString(),
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(Object.fromEntries(formData)).toString(),
         })
-        .then(() => alert("Form successfully submitted"))
-    .catch((error) => alert(error));
+          .then(() =>{
+            setDisplayForm(false);
+                    setEmailSent(true)
+          } )
+          .catch((error) => {
+            console.log(error);
+            setEmailSent(false);
+            setDisplayForm(true)
+          } );
 
-    //   emailjs.sendForm('service_k44p1gp', 'template_g3hde9g', form.current, 'umsUa9FrGA1jU2Ck3')
-    //     .then((result) => {
-    //         console.log(result.text);
-    //         setDisplayForm(false);
-    //         setEmailSent(true)
-    //     }, (error) => {
-    //         console.log(error.text);
-    //         setEmailSent(false);
-    //         setDisplayForm(true)
-    //     });
-       
     }
     return(
         <section className="container__contactForm">
@@ -42,24 +36,15 @@ const ContactForm = () => {
                 <p>You want to get in touch?<br/>You're welcome to contact us via this form for any inquiries related to A.C. Ventures. We will be glad to hear from you and we'll make sure to get back to you as soon as possible. <br/></p>
             </div>
             {(displayForm && emailSent === false) ? (
-            // <form ref={form} onSubmit={sendEmail}>
-            //     <input type={"text"} placeholder={"First Name"} name="user_name" required/>
-            //     <input type={"text"} placeholder={"Last Name"} name="user_lastName" required/>
-            //     <input type={"email"} placeholder={"Your Email"} name="user_email" required/>
-            //     <input type={"tel"} placeholder={"Phone Number"} name="user_tel" />
-            //     <input type={"text"} placeholder={"Subject"} name="user_subject" />
-            //     <textarea name="message" id="" cols="30" rows="10" placeholder={"Your Message"} required></textarea>
-            //     <button className="submit-btn" type="submit"><span>Send the message</span><img src="/assets/icons/Union.svg" alt="Arrow going right" /></button>
-            // </form>
-            <form name="contact" method="POST" data-netlify="true" onSubmit={sendEmail}>
-                {/* <input type="hidden" name="contact-ACVentures" value="name_of_my_form" /> */}
-                <input type="text" placeholder={"First Name"} />
-                <input type={"text"} placeholder={"Last Name"} name="user_lastName" required/>
-                <input type={"email"} placeholder={"Your Email"} name="user_email" required/>
-                 <input type={"tel"} placeholder={"Phone Number"} name="phone" />
-                 <input type={"text"} placeholder={"Subject"} name="user_subject" />
-                 <textarea name="message" id="" cols="30" rows="10" placeholder={"Your Message"} required></textarea>
-                 <button className="submit-btn" type="submit"><span>Send the message</span><img src="/assets/icons/Union.svg" alt="Arrow going right" /></button>
+            <form name="contact" onSubmit={handleSubmit} method="POST" data-netlify="true" netlify>
+                <input type={"text"} placeholder={"First Name"} name="name" required/>
+                <input type={"text"} placeholder={"Last Name"} name="lastName" required/>
+                <input type={"email"} placeholder={"Your Email"} name="email" required/>
+                <input type={"tel"} placeholder={"Phone Number"} name="tel" />
+                <input type={"text"} placeholder={"Subject"} name="subject" />
+                <textarea name="message" id="" cols="30" rows="10" placeholder={"Your Message"} required></textarea>
+                <button className="submit-btn" type="submit"><span>Send the message</span><img src="/assets/icons/Union.svg" alt="Arrow going right" /></button>
+                <input type="hidden" name="form-name" value="contact" hidden />
             </form>
             ) : (
                 <div className="form-thanks">
